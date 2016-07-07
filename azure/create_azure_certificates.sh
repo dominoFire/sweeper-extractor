16b3f4acdb3eed547ac6bd8b5c3da0eee8af76e0
Fernando Aguilar <fer.aguilar.reyes@gmail.com>
2016-07-07 18:42:35 -0500

    Tweaks a la generacion de certificados

--- azure/create_azure_certificates.sh
+++ azure/create_azure_certificates.sh
@@ -3,9 +3,10 @@
 set -e
 #set -v
 
+CERT_NAME=service_certificate
+
 # creates private key
-#openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mycert.pem -out mycert.pem
-openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout mycert.pem -out mycert.pem <<EOF
+openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout $CERT_NAME.pem -out $CERT_NAME.pem <<EOF
 MX
 Mexico
 Mexico
@@ -14,9 +15,9 @@ RDU
 lenovo
 fernando.aguilar@pulsarcloud.ml
 EOF
+
 # creates certificate file (public key) in DER
-#openssl x509 -inform pem -in mycert.pem -outform der -out mycert.cer
-openssl x509 -inform pem -in mycert.pem -outform der -out mycert.cer
+openssl x509 -inform pem -in $CERT_NAME.pem -outform der -out $CERT_NAME.cer
 
 # https://www.lisenet.com/2014/convert-p7b-to-pfx-with-openssl/
 #openssl req -new -x509 -days 365 -sha256 -nodes -out cert.cer -keyout cert.key
